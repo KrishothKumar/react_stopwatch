af080c8c85d91f7e7a039e0f31e3a368aa042cfb
Krishoth Kumar <Krishoth@ksatech.com.au>
2023-09-03 13:05:58 +0530

    Stop Watch initial Commit.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  
+  const [mileSecond, setMileSecond] = useState(0)
+  const [second, setSecond] = useState(0)
+  const [minutes, setMinutes] = useState(0)
+  const [hours, setHours] = useState(0)
+
+  const handleStart = () => {
+    setMileSecond()
+  }
+  
+  const calculateMileSecond = () =>{
+
+  }
+  
+  const calculateSecond = () =>{
+    
+  }
+  
+  const calculateMinutes = () =>{
+    
+  }
+  
+  const calculateHours = () =>{
+    
+  }
+
+  const handleStop = () => {
+    console.log('Stop')
+  }
+  
+  const handleReset = () => {
+    setMileSecond(0)
+    setSecond(0)
+    setMinutes(0)
+    setHours(0)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{(hours > 0) ? `${hours} h`: null} {(hours > 0 || minutes > 0) ? `${minutes} m` : null } {second} s {mileSecond}</p>
+      <button onClick={handleStart}>Start</button>
+      <button onClick={handleStop}>Stop</button>
+      <button onClick={handleReset}>Reset</button>
     </div>
   );
 }
