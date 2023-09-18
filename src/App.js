74fc8f38500b9c9caa73ae74242be3db1dcb0f85
Krishoth Kumar <Krishoth@ksatech.com.au>
2023-09-18 12:48:13 +0530

    Added CSS and useCallback changes.

--- src/App.js
+++ src/App.js
@@ -5,19 +5,25 @@ function App() {
   
   const [isStart, setIsStart] = useState(false)
   // const [start, setStart] = useState()
-  const [mileSecond, setMileSecond] = useState(0)
+  const [mileSecond, setMileSecond] = useState(60000)
   const [lap, setLap] = useState([])
   const interval = useRef(null)
-  
+  // var interval
+
   // Initial Code
-  // function startWatch() {
-  //   setStart(setInterval(function(){ 
-  //     setMileSecond((mileSecond) => mileSecond + 10)
-  //   }, 10))
-  // } 
-  // function stopWatch() {
-  //   clearInterval(start)
-  // }
+  function startWatch() {
+    setIsStart(true)
+    interval.current = setInterval(function(){ 
+      setMileSecond((mileSecond) => mileSecond + 10)
+    }, 10)
+  } 
+
+  function stopWatch() {
+    setIsStart(false)
+    clearInterval(interval.current)
+    interval.current = null
+    // clearInterval(interval)
+  }
 
   const startInterval = useCallback(() => {
     setIsStart(true)
@@ -32,34 +38,32 @@ function App() {
   })
 
   const addLap = () => {  
-    const previousLap = lap.length == 0 ? lap[lap.length-1] : 0
-    const add = [{lap:lap.length, previousLap:previousLap, mileSecond:mileSecond}, ...lap]
+    console.log(lap.length,lap)
+    const previousLap = (lap.length > 0) ? lap[0].mileSecond : 0
+    const add = [{lap:lap.length+1, previousLap:previousLap, mileSecond:mileSecond}, ...lap]
     setLap(add)
   }
   
   const handleReset = () => {
-    // stopWatch()
-    stopInterval()
+    stopWatch()
     setIsStart(false)
     setMileSecond(0)
     setLap([])
   }
   return (
     <div className="App">
-      <p>
-        { (Math.floor((mileSecond/1000)/(60*60))%60 > 0) ? `${Math.floor((mileSecond/1000)/(60*60))%60} h `: null } 
-        { (Math.floor((mileSecond/1000)/60) > 0) ? `${Math.floor((mileSecond/1000)/60)%60} m ` : null } 
-        { `${Math.floor(mileSecond/1000)%60} s ` } 
-        { mileSecond%100 }
-      </p>
-      <button style={ (isStart === true) ? {display: 'none'} : null} onClick={startInterval}>Start</button>
-      <button style={ (isStart == false) ? {display: 'none'} :  null} onClick={stopInterval}>Stop</button>
+        <p className='stop-watch'>{ (Math.floor((mileSecond/1000)/(60*60))%60 > 0) ? `${Math.floor((mileSecond/1000)/(60*60))%60} h `: null }</p>
+        <p className='stop-watch'>{ (Math.floor((mileSecond/1000)/60) > 0) ? `${Math.floor((mileSecond/1000)/60)%60} m ` : null } </p>
+        <p className='stop-watch'>{ `${Math.floor(mileSecond/1000)%60} s ` }</p> 
+        <p className=''>{ mileSecond%100 }</p>
+      <button style={ (isStart === true) ? {display: 'none'} : null} onClick={startWatch}>Start</button>
+      <button style={ (isStart == false) ? {display: 'none'} :  null} onClick={stopWatch}>Stop</button>
       <button style={ (mileSecond == 0) ? {display: 'none'} :  null} onClick={handleReset}>Reset</button>
       <button style={ (mileSecond == 0) ? {display: 'none'} :  null} onClick={addLap}>Lap</button>
       
       {lap.map((lap) => (
           <p>
-            {`#${lap.lap + 1}  `}
+            {`#${lap.lap} `}
             {` ${Math.floor((lap.mileSecond/1000)/(60*60))%60} h `} 
             {`${Math.floor((lap.mileSecond/1000)/60)%60} m `} 
             { `${Math.floor(lap.mileSecond/1000)%60} s ` } 
