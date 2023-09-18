333d2a92902ce1812087750ca99faf3c90c8b6e7
Krishoth Kumar <Krishoth@ksatech.com.au>
2023-09-18 13:30:29 +0530

    FIXED: Mileseconds counting.

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ function App() {
         <p className='stop-watch'>{ (Math.floor((mileSecond/1000)/(60*60))%60 > 0) ? `${Math.floor((mileSecond/1000)/(60*60))%60} h `: null }</p>
         <p className='stop-watch'>{ (Math.floor((mileSecond/1000)/60) > 0) ? `${Math.floor((mileSecond/1000)/60)%60} m ` : null } </p>
         <p className='stop-watch'>{ `${Math.floor(mileSecond/1000)%60} s ` }</p> 
-        <p className=''>{ mileSecond%100 }</p>
+        <p className=''>{ mileSecond%99 }</p>
       <button style={ (isStart === true) ? {display: 'none'} : null} onClick={startWatch}>Start</button>
       <button style={ (isStart === false) ? {display: 'none'} :  null} onClick={stopWatch}>Stop</button>
       <button style={ (mileSecond === 0) ? {display: 'none'} :  null} onClick={handleReset}>Reset</button>
